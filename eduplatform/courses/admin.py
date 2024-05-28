25faefd5dbdcdb4c2c893874edb37fa6b9b766c8
Yana <yana.71092gr@gmail.com>
2024-05-28 16:04:37 +0300

    create models course and group

--- eduplatform/courses/admin.py
+++ eduplatform/courses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Specialization, Course, Group
 
-# Register your models here.
+admin.site.register(Specialization)
+admin.site.register(Course)
+admin.site.register(Group)
--- eduplatform/courses/models.py
+++ eduplatform/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import Student, Teacher
 
 
 class Specialization(models.Model):
@@ -11,3 +12,35 @@ class Specialization(models.Model):
 
     def __str__(self):
         return f'Specialization #{self.pk} - {self.title}'
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    duration = models.DurationField(verbose_name="Продолжительность")
+    specialization = models.ManyToManyField(Specialization)
+
+    def str(self):
+        return f"{self.pk} - {self.title}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ['id']
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название")
+    quantity = models.IntegerField(verbose_name="Количество человек")
+    date_formation = models.DateField(verbose_name="Дата образования")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def str(self):
+        return f"{self.pk} - {self.title} - {self.date_formation}"
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+        ordering = ['id']
