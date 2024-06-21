50cf9c00aae9cca5c008b7125dcd3ffab9e23b6b
Yuri <yura.shurhay@gmail.com>
2024-06-21 08:05:00 +0300

    Add secret info to .env

--- eduplatform/eduplatform/settings.py
+++ eduplatform/eduplatform/settings.py
@@ -141,8 +141,8 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "yura.shurhay@gmail.com"
-EMAIL_HOST_PASSWORD = "cfveunelmallxffx"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "yura.shurhay@gmail.com"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
